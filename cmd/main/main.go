6fd826391d5af8a59cbe9795c7a74f9b8da20475
MonMon201 <danyavasilinenko213@gmail.com>
2020-12-23 23:44:02 +0200

    add commands and main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"bufio"
+	"os"
+
+	"github.com/G-V-G/l4/engine"
+)
+
 func main() {
-	
+	filename := "commands.txt"
+	eventLoop := new(engine.Engine)
+	eventLoop.Start()
+	if input, err := os.Open(filename); err == nil {
+		defer input.Close()
+		scanner := bufio.NewScanner(input)
+		for scanner.Scan() {
+			commandLine := scanner.Text()
+			cmd := parse(commandLine)
+			eventLoop.Post(cmd)
+		}
+	} else {
+		panic(err)
+	}
+	eventLoop.AwaitFinish()
 }
