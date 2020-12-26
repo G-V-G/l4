070f1d34e88d0a02f760192b61d9ddc52f6b448d
Andrew1407 <endry1407@gmail.com>
2020-12-26 02:51:59 +0200

    engine: start method (on finish check)

--- engine/engine.go
+++ engine/engine.go
@@ -1,5 +1,7 @@
 package engine
 
+import "sync"
+
 type onFinishFn func (h Handler)
 
 func (fn onFinishFn) Execute (h Handler) {
@@ -8,9 +10,16 @@ func (fn onFinishFn) Execute (h Handler) {
 
 // Engine - loop handler
 type Engine struct {
+	sync.Mutex
 	onFinishChan chan bool
 	stopRequest bool
 	cmdStack *Queue
+}
+
+func (e *Engine) isFinishedSync() bool {
+	e.Lock()
+	defer e.Unlock()
+	return e.stopRequest && len(e.cmdStack.Commands) == 0
 }
 
 // Start runs main loop
@@ -18,9 +27,12 @@ func (e *Engine) Start() {
 	e.cmdStack = &Queue{onReceiveEmptyChan: make(chan bool)}
 	e.onFinishChan = make(chan bool)
 	go func() {
-		for !e.stopRequest || len(e.cmdStack.Commands) != 0 {
+		for {
 			cmd := e.cmdStack.Pull()
 			cmd.Execute(e)
+			if e.isFinishedSync() {
+				break
+			}
 		}
 		e.onFinishChan <- true
 	}()
